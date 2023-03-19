c44816469adb6afad22dba0aecad5d821ac92bb8
mrasm <mrasm@DESKTOP-3QBF7AU>
2023-03-19 18:18:26 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int satir = 3;
+            int sutun = 3;
+
+            for (int i = 1; i <= satir; i++)
+            {
+                for (int j = 1; j <= sutun; j++)
+                {
+                    // matrisin köşelerini oluşturmak için " * " sembolünü kullanmak için koşul belirtimi
+                    if (i == 1 && j == 1 || i == 1 && j == sutun || i == satir && j == 1 || i == satir && j == sutun)
+                    {
+                        Console.Write("*");
+                    }
+                    // matrisin çervesini oluşturmak için " - " harfini kullanmak için koşul belirtimi
+                    else if (i == 1 || i == satir || j == 1 || j == sutun)
+                    {
+                        Console.Write("-");
+                    }
+                    // matrisin içini oluşturmak için kullanıyoruz
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
